e8114ce69c44790df1febb180d43680a22ff361f
Lavanya <Lavanya@Amma>
2018-04-25 13:39:25 +0530

    Third 25-Commit

--- src/generic/AutoConstant.java
+++ src/generic/AutoConstant.java
@@ -4,9 +4,7 @@ public interface AutoConstant
 {
 	String chrome_key="Webdriver.chromedriver.driver";
 	String chrome_value="./drivers/chromedriver.exe";
-	
 	String gecko_key="Webdriver.gecko.driver";
 	String gecko_value="./drivers/geckodriver.exe";
-	
 	String file_path="./data/input2.xlsx";
 }
